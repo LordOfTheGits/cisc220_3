b949bec8e7dbeee86da1ca7e11239f9e2e7dda85
mattrodgers <15mmr@queensu.ca>
2016-11-12 19:49:36 -0500

    Update blockSort.sh

--- blockSort.sh
+++ blockSort.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-lsblk --noheadings --raw | sed s/"K"/" K"/ | sed s/"M"/" M"/ | sed s/"G"/" G"/ | sort -k5,2 -k4,1n
+lsblk --noheadings --raw | sed s/"K"/" K"/ | sed s/"M"/" M"/ | sed s/"G"/" Z"/ | sort -k5,5d -k4,4n | sed s/" K"/"K"/ | sed s/" M"/"M"/ | sed s/" Z"/"G"/
