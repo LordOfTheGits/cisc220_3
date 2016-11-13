584b4c70bf843d993c7e13ba56420094170527b2
mattrodgers <15mmr@queensu.ca>
2016-11-13 00:42:52 -0500

    Code cleanup and fix indexing

--- developedAdder.sh
+++ developedAdder.sh
@@ -3,30 +3,20 @@
 perms=($(ls -l | awk '{print $1}')) #array with permisions for all files in current dir
 names=($(ls -l | awk '{print $9}')) #array with names of all files in current dir
 
-len=${#perms[*]} #number of files in current dir
-
-str="#!/bin/bash"  
+len=${#names[*]} #number of files in current dir
 
 #for loop that will go through all the files in the current directory
-for (( i=1 ; i<len ; i++ )) ; do
+for (( i=0 ; i<len ; i++ )) ; do
 
 	#ex will have a value of 0 if the file is not executable
-	ex=$(expr index "${perms[i]}" "x")
+	ex=$(expr index "${perms[$((i+1))]}" "x")
 
 	#if the file is executable the find the #!/bin/bash line
 	#incase it isn't the first line
 	if [[ ! $ex == 0 ]]; then
 
-		#loc to tell if the file has a #!/bin/bash line
-		loc=$(grep -n "$str" ${names[i]})
-		loc=${loc:0:1}
-
-		if  [[ $loc == 1 ]]; then
-
-			#sed commmand to add "#Developed in cisc220" after the #!/bin/bash line
-			sed -i '1 a\#Developed in cisc220' ${names[i]} ;
-
-		fi; #end if
+		#sed commmand to add "#Developed in cisc220" after the #!/bin/bash line
+		sed -i '1 a\#Developed in cisc220' ${names[i]} ;
 
 	fi #end if
 
