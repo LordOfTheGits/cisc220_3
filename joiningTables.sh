d1e4fb76b7bc70936411e796520a72683bdd955b
b0chen <bochen@live.ca>
2016-11-13 21:56:57 -0500

    Update joiningTables.sh

--- joiningTables.sh
+++ joiningTables.sh
@@ -7,10 +7,22 @@ awk 'NR > 1' student_course.txt | sort > sStudentCourse
 join sStudent sStudentCourse | awk '{print("%s %s\n", $3, $1, $2)}' | sort > list
 join list sCourse > sList
 
-echo "Sorted by Student" 
-echo "studentName courseName" 
-awk '{print $3 " " $4 }' sList | sort
+clear
 
-echo "Sorted by Course" 
-echo "courseName studentName" 
-awk '{print  $4  " " $3 }' sList | sort
+echo ""
+echo " --- Sorted by Student ---  " 
+echo "|--------------------------|" 
+echo "| studentName | courseName |" 
+echo "|--------------------------|" 
+awk '{print "|  " $3 "\t" "      | " $4 "\t" "   |" }' sList | sort
+echo "|--------------------------|" 
+echo ""
+
+echo ""
+echo "  --- Sorted by Course ---  " 
+echo "|--------------------------|" 
+echo "| courseName | studentName |" 
+echo "|--------------------------|" 
+awk '{print "|  " $4 "   |  " $3 "\t" "   |"}' sList | sort
+echo "|--------------------------|" 
+echo ""
